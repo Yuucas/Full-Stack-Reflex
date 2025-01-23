d6fe192bb1b2ed52caf81e519ebf15f500a42557
Yükselcan Sevil <yukselcan.96@gmail.com>
2025-01-23 22:33:22 +0300

    Navbar
    It shows how to add navbar and how to hide navbar.

--- Full_Stack_Reflex/Full_Stack_Reflex.py
+++ Full_Stack_Reflex/Full_Stack_Reflex.py
@@ -12,20 +12,32 @@ class State(rx.State):
     def handle_title_input_change(self, val):
         self.label = val
 
-def base_page(*args, **kwargs) -> rx.Component:
+def navbar() -> rx.Component:
+    return rx.heading("SaaS", size="6"),
+
+def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
     # Print the type of the arguments in the base page
     print([type(x) for x in args])
+    # Check if the child is a component
+    if not isinstance(child, rx.Component):
+        child = rx.heading("This is not a valid child element")
+    if hide_navbar:
+        return (
+            child,
+            rx.logo(),
+            rx.color_mode.button(position="bottom-left"),
+        )
+    # Child: is used to call the components of the container in every page
     return rx.container(
-        *args,
+        child,
+        navbar(),
         rx.logo(),
         rx.color_mode.button(position="bottom-left"),
     )
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return base_page(
         rx.vstack(
-            rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
@@ -44,6 +56,7 @@ def index() -> rx.Component:
             justify="center",
             min_height="85vh",
         ),
+        hide_navbar=True
     )
 
 
