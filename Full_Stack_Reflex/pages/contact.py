561e0271f548a939feb3a3282190bd8ade127c6a
Yükselcan Sevil <yukselcan.96@gmail.com>
2025-01-27 17:14:16 +0300

    Contact Form
    Creating simple contact form

--- Full_Stack_Reflex/pages/contact.py
+++ Full_Stack_Reflex/pages/contact.py
@@ -3,16 +3,50 @@ from .. import navigation
 from ..ui.base import base_page
 
 
+class ContactState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        print(form_data)
+        self.form_data = form_data
+
 @rx.page(route=navigation.routes.CONTACT_ROUTE)
 def contact_page() -> rx.Component:
-
+    contact_form = rx.form(
+                rx.vstack(
+                        rx.input(
+                            name="first_name",
+                            placeholder="First Name",
+                            type='text',
+                            required=True,
+                        ),
+                        rx.input(
+                            name="last_name",
+                            placeholder="Last Name",
+                            type='text',
+                        ),
+                        rx.input(
+                            name="email",
+                            placeholder="Email",
+                            type='email',
+                            required=True,
+                        ),
+                        rx.text_area(
+                            name='messgae',
+                            placeholder='Message',
+                            type='text',
+                            required=True,
+                        ),
+                        rx.button("Submit", type="submit"),
+                        align='center',
+                    ),
+                    on_submit=ContactState.handle_submit,
+                    reset_on_submit=True,
+            ),
     my_child = rx.vstack(
             rx.heading("Contact Page", size="9"),
-            rx.text(
-                "There is a contact list",
-                color_scheme='bronze'
-            ),
-            spacing="5",
+            contact_form,
+            spacing="7",
             justify="center",
             align='center',
             text_align='center',
