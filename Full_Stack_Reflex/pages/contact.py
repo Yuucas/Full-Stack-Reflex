f594b3bcac9cd6bdfc8c9ef3dbe147e687b4f2f7
Yükselcan Sevil <yukselcan.96@gmail.com>
2025-01-27 17:33:55 +0300

    Contact Form Updated
    Better version of contact form.

--- Full_Stack_Reflex/pages/contact.py
+++ Full_Stack_Reflex/pages/contact.py
@@ -14,38 +14,60 @@ class ContactState(rx.State):
 def contact_page() -> rx.Component:
     contact_form = rx.form(
                 rx.vstack(
-                        rx.input(
-                            name="first_name",
-                            placeholder="First Name",
-                            type='text',
-                            required=True,
-                        ),
-                        rx.input(
-                            name="last_name",
-                            placeholder="Last Name",
-                            type='text',
+                        rx.hstack(
+                            rx.input(
+                                name="first_name",
+                                placeholder="First Name",
+                                type='text',
+                                required=True,
+                                width='100%'
+                            ),
+                            rx.input(
+                                name="last_name",
+                                placeholder="Last Name",
+                                type='text',
+                                width='100%'
+                            ),
+                            width='100%',
+                            align='stretch'
                         ),
                         rx.input(
                             name="email",
                             placeholder="Email",
                             type='email',
                             required=True,
+                            width='100%'
                         ),
                         rx.text_area(
-                            name='messgae',
+                            name='message',
                             placeholder='Message',
                             type='text',
                             required=True,
+                            width='100%',
+                            height='16vh'
                         ),
                         rx.button("Submit", type="submit"),
-                        align='center',
+                        # align='center',
                     ),
                     on_submit=ContactState.handle_submit,
                     reset_on_submit=True,
             ),
     my_child = rx.vstack(
             rx.heading("Contact Page", size="9"),
-            contact_form,
+            rx.desktop_only(
+                rx.box(
+                    contact_form,
+                    id='contact_form_desktop',
+                    width='40vw',
+                )   
+            ),
+            rx.mobile_and_tablet(
+                rx.box(
+                    contact_form,
+                    id='contact_form_mobile',
+                    width='75vw',
+                )
+            ),
             spacing="7",
             justify="center",
             align='center',
