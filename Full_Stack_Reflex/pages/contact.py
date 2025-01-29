c8fa04fcab7a5c6fe509d20c47b55d9c07def8c0
Yükselcan Sevil <yukselcan.96@gmail.com>
2025-01-29 10:22:31 +0300

    Model Field Changes and Migrations
    Added nullable types to table. Also timestamp added to table.

--- Full_Stack_Reflex/pages/contact.py
+++ Full_Stack_Reflex/pages/contact.py
@@ -1,15 +1,35 @@
-import time
+from datetime import datetime, timezone
 import asyncio
 import reflex as rx
+
+import sqlalchemy
+from sqlmodel import Field
+
 from .. import navigation
 from ..ui.base import base_page
 
+def get_utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
 class ContacEntryModel(rx.Model, table=True):
+    '''
+    There is two way to do the string nullable. 
+    The first one is --> string: str | None = None
+    The second one is --> string: str = Field(nullable=True)
+    '''
     # Define scheme
     first_name: str
-    last_name: str
-    email: str
+    last_name: str | None = None
+    email: str = Field(nullable=True)
     message: str
+    create_date: datetime = Field(
+        default_factory=get_utc_now,
+        sa_type=sqlalchemy.DateTime(timezone=True),
+        sa_column_kwargs={
+            "server_default": sqlalchemy.func.now()
+        },
+        nullable=False
+        )
 
 
 class ContactState(rx.State):
